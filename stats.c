8d4dc9a371409120662e1b450ed3325e0ab0cb42
Mostafa Said <mostafamostafa741@hotmail.com>
2018-01-19 19:27:37 +0200

    adding code outline and documentaion for C1M1

--- stats.c
+++ stats.c
@@ -2,14 +2,14 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
+ * @file <Add File Name>
  * @brief <Add Brief Description Here >
  *
  * <Add Extended Description Here>
@@ -36,8 +36,32 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+
   /* Statistics and Printing Functions Go Here */
+  print_array(test,SIZE);
+  print_statistics(test,SIZE);
+  print_array(test,SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
+void print_statistics(unsigned char array[],unsigned int length){
+}
+
+void print_array(unsigned char array[],unsigned int length){
+}
+
+unsigned char find_median(unsigned char array[],unsigned int length){
+}
+
+unsigned int find_mean(unsigned char array[],unsigned int length){
+}
+
+unsigned char find_maximum(unsigned char array[],unsigned int length){
+}
+
+unsigned char find_minimum(unsigned char array[],unsigned int length){
+}
+
+void sort_array(unsigned char array[],unsigned int length){
+}
--- stats.h
+++ stats.h
@@ -2,39 +2,112 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief get some statstics of an array
  *
- * <Add Extended Description Here>
+ * get some statstics of an array such as maximum, minimum, mean, and median.
+ * also print the array.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mostafa Said
+ * @date 19/1/2018
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints some statstics of an array
  *
- * <Add Extended Description Here>
+ * Take a pointer to unsigned char array and sort array descendingly
+ * also prints minimum,maximum,mean,and median
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param array pointer to unsigned char array
+ * @param length total number of elements
  *
- * @return <Add Return Informaiton here>
  */
+ void print_statistics(unsigned char array[],unsigned int length);
 
+ /**
+ * @brief prints an array
+ *
+ * prints an array in nicely format
+ * number[index of the number]
+ * 
+ * @param array unsigned char array intended to print nicely formarted :)
+ * @param length unsigned int total number of elements
+ */
+ void print_array(unsigned char array[],unsigned int length);
+
+/**
+ * @brief find median of an array
+ *
+ * find median of an array sorded DESCENDINGLY.
+ * if number of elements is odd then it must be the middle one 
+ * example array of 7 elements it must the 4th element
+ * 
+ * if number of elements is even then it must be the middle one + the one after it divided by 2
+ * example array of 8 elements it must the 4th element + the 5th element 
+ *
+ * @param array unsigned char array intended to get median of it, IT MUST BE SORTED DESCENDINGLY
+ * @param length unsigned int total number of elements
+ *
+ * @return unsigned char median
+ */
+ unsigned char find_median(unsigned char array[],unsigned int length);
+
+/**
+ * @brief get mean of an array
+ *
+ * get mean of an array by getting the summation of the array divided by the number of elements. 
+ *
+ * @param array unsigned char array intended to get mean of it
+ * @param length unsigned int total number of elements
+ *
+ * @return unsigned char mean
+ */
+ unsigned int find_mean(unsigned char array[],unsigned int length);
+
+/**
+ * @brief getting the biggest number of an array
+ *
+ * getting the biggest number of sorted descendingly array, it just return the first element
+ *
+ * @param array unsigned char array intended to get maximum of it, IT MUST BE SORTED DESCENDINGLY
+ * @param length unsigned int total number of elements
+ *
+ * @return unsigned char maximum
+ */
+ unsigned char find_maximum(unsigned char array[],unsigned int length);
+
+/**
+ * @brief  getting the smallest number of an array
+ *
+ * getting the smallest number of sorted descendingly array, it just return the last element
+ *
+ * @param array unsigned char array intended to get minimum of it, IT MUST BE SORTED DESCENDINGLY
+ * @param length unsigned int total number of elements
+ *
+ * @return unsigned char minimum
+ */
+ unsigned char find_minimum(unsigned char array[],unsigned int length);
+
+/**
+ * @brief sort array descendingly
+ *
+ * sort array descendingly by using bubble sort algorithm 
+ *
+ * @param array unsigned char array intended to sort it DESCENDINGLY
+ * @param length unsigned int total number of elements
+ */
+ void sort_array(unsigned char array[],unsigned int length);
 
 #endif /* __STATS_H__ */
